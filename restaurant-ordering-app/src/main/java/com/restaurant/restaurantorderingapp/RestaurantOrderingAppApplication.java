5cf3972513c8232f4bd4739815b6a3db4a946e17
Liubre82 <liubre82@students.rowan.edu>
2024-03-08 23:41:51 -0500

    inital commit

---
+++ restaurant-ordering-app/src/main/java/com/restaurant/restaurantorderingapp/RestaurantOrderingAppApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.restaurantorderingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantOrderingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantOrderingAppApplication.class, args);
+	}
+
+}
