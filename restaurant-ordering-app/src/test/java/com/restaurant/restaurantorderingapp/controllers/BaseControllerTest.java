4fc392e38fcc7f4f1adc29e82c1e8bacf4bb4076
Liubre82 <liubre82@students.rowan.edu>
2024-04-16 02:19:45 -0400

    attempt to fix jenkins error 4

--- restaurant-ordering-app/src/test/java/com/restaurant/restaurantorderingapp/controllers/BaseControllerTest.java
+++ restaurant-ordering-app/src/test/java/com/restaurant/restaurantorderingapp/controllers/BaseControllerTest.java
@@ -1,13 +1,10 @@
 package com.restaurant.restaurantorderingapp.controllers;
 
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
@@ -17,8 +14,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@ExtendWith(SpringExtension.class)
-@ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters  = false)
 public abstract class BaseControllerTest {
 
